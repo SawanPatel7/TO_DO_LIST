76c7dc40c3f75768cf68bc5d4a9ad4827e3919f3
sawan patel <sawanp06@gmail.com>
2021-08-26 12:12:04 +0530

    Add git ignore

--- app.js
+++ app.js
@@ -81,7 +81,11 @@ app.post("/", function (req, res) {
     res.redirect("/");
 
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("server started at port 3000");
 });
